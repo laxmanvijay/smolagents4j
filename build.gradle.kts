b7a638696a5353b8a1b4c54f1980e08bcf754472
Laxman Vijay <laxmanvijay24@gmail.com>
2026-03-15 19:18:08 +0100

    added spring ai

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,8 @@ repositories {
 }
 
 dependencies {
+    implementation(platform("org.springframework.ai:spring-ai-bom:1.0.0"))
+    implementation("org.springframework.ai:spring-ai-openai")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
